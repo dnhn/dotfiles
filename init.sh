21407576cb52f89f17a18ef44e4bfcd1742215de
Nhan Doan <2561973+nhantdn@users.noreply.github.com>
2016-08-28 06:00:09 +0700

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,25 @@
+### Apps
+# Chrome, FileZilla, Atom, PhpStorm, Font Manager
+
+### Fonts
+# Lato, Open Sans, Open Sans Condensed, Raleway, Roboto, Roboto Condensed,
+# Source Sans Pro, Ubuntu
+
+### Git
+sudo apt install git
+
+### APT install Node 4
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+### Update latest NPM and install packages
+sudo npm install -g npm
+sudo npm install -g bower browserify generator-webapp grunt gulp npm-check psi watchify webpack yo
+
+### SSH, bridged network on VM | ssh user@ipaddress:port
+sudo apt-get install openssh-server
+
+### Synapse app launcher
+sudo add-apt-repository ppa:synapse-core/ppa 
+sudo apt-get update
+sudo apt-get install synapse
