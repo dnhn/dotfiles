bc362ba0d0dc9ef73ca450c44e21755d5b722436
Nhan Doan <2561973+nhantdn@users.noreply.github.com>
2018-06-27 23:19:36 +0700

    Update NPM packages

--- npm.sh
+++ npm.sh
@@ -6,16 +6,19 @@ packages=(
   bower
   cordova
   create-react-app
+  create-react-native-app
   express-generator
   generator-server-configs
   generator-webapp
   github-email
   grunt
   gulp
+  ignite-cli
   ionic
   ios-deploy
   isogram
   lighthouse
+  loopback-cli
   npm
   npm-check
   parcel-bundler
@@ -34,5 +37,5 @@ npm install -g ${packages[@]}
 
 # Special case for foundation-cli
 # Reinstallation of existing package causes errors for other global packages.
-npm uninstall -g foundation-cli && \
-npm install -g foundation-cli
+# npm uninstall -g foundation-cli && \
+# npm install -g foundation-cli
