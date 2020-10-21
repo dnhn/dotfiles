f6d2f5145c712b0f3146c537f152126c6ff26d1b
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2020-10-22 02:11:10 +0700

    Git init script

---
+++ git/init.sh
@@ -0,0 +1,9 @@
+cp .git-alias ~/.git-alias
+git config --global color.ui true
+git config --global core.editor vim
+git config --global core.eol lf
+git config --global diff.tool vimdiff
+git config --global include.path ~/.git-alias
+git config --global init.defaultBranch root
+git config --global instaweb.httpd webrick
+git config --global push.default current
