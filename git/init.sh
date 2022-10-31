b5ee9d222786e371c4a5e365162169c244c5dad6
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2022-11-01 05:54:00 +0700

    Update shell commands and VSCode

--- git/init.sh
+++ git/init.sh
@@ -1,4 +1,4 @@
 cp .git-alias ~/.git-alias
 cp .gitconfig-custom ~/.gitconfig-custom
-git config --global include.path ~/.git-alias
-git config --global include.path ~/.gitconfig-custom
+git config --global --add include.path ~/.git-alias
+git config --global --add include.path ~/.gitconfig-custom
